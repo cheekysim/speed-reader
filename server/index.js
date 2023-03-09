5a34504475ae1dfdb65d2d2a76a2ed5204ecfaa9
Tyler <68140692+Tyler2P@users.noreply.github.com>
2023-03-09 21:46:26 +0000

    Added API versioning & 404 pages

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const app = express();
+const { supportedVersions } = require("./config.json");
+
+// Include different API versions
+app.use(require("./v1/api"));
+
+app.all("*", function(req, res) {
+  // Redirect the user to the repository
+  if (!(req.path).includes("/api/"))
+    return res.status(302).redirect("https://github.com/cheekysim/speed-reader");
+  // Return a 404 message
+  res.status(404).json({ error: "Page not found" });
+});
 
 app.listen(3002, () => console.log("Server is online"));
---
+++ server/v1/api.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.get("/api/v1/", function(req, res) {
+
+});
+
+module.exports = router;
