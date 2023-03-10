b9911f42ec079eb8ae48724a11aa5980340ec0cc
Tyler <tylerrp06@gmail.com>
2023-03-10 18:24:48 +0000

    Made progress towards API
    * Created a blank canvas
    * Added new imports
    * Added support for different themes

--- src/server/v1/api.ts
+++ src/server/v1/api.ts
@@ -1,7 +1,24 @@
 import express, { Request, Response } from "express";
+import { createCanvas } from "canvas";
+import GIFEncoder from "gifencoder";
+import fs from "fs";
+
 const router = express.Router();
 
-router.get("/api/v1/", function(req: Request, res: Response) {
+router.get("/api/v1/create", function(req: Request, res: Response) {
+  // Create a canvas
+  const canvas = createCanvas(400, 200);
+  const ctx = canvas.getContext("2d");
+
+  // Define variables
+  let theme = "dark";
+
+  if (!req.body.words)
+    return res.status(400).json({ error: "Words must be provided" });
+  if (["light", "dark"].includes(req.body.theme))
+    theme = req.body.theme;
+  
+
   res.status(200).send("Hello, World!");
 });
 
