c0300e03d4ad0c5b06e8a7a4bd1fa4f53a6740c1
Tyler <tylerrp06@gmail.com>
2023-03-10 17:50:48 +0000

    Added usage of config.json
    Changed from constant variables to variables from within the config.json file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import api from "./v1/api.js"
+import config from "./config.json" assert { type: "json" };
 const app = express();
-
-const port = 3002;
-const host = "localhost";
 
 // Include different API versions
 app.use(api);
 
-app.all("*", function(req, res) {
+// Handle 404 requests
+app.all("*", function(req: Request, res: Response) {
   // Redirect the user to the repository
   if (!(req.path).includes("/api/"))
     return res.status(302).redirect("https://github.com/cheekysim/speed-reader");
@@ -16,4 +15,4 @@ app.all("*", function(req, res) {
   res.status(404).json({ error: "Page not found" });
 });
 
-app.listen(port, () => console.log(`Server running at http://${host}:${port}`));
+app.listen(config.port, () => console.log(`Server running at http://${config.host}:${config.port}`));
